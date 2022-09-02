7dd10815bde535c4a698a6145d8dbf3fc3318dc2
lel99999 <luan.le@cfpb.gov>
2022-09-01 22:16:36 -0400

    add feature ideas

--- listpackages.R
+++ listpackages.R
@@ -6,3 +6,19 @@ pkg = tibble::tibble(
   Package = names(installed.packages()[,3]),
   Version = unname(installed.packages()[,3])
 )
+
+write(pkg, "", sep = ", ")
+
+# create a 2 by 5 matrix
+x <- matrix(1:10, ncol = 5)
+
+fil <- tempfile("data")
+# the file data contains x, two rows, five cols
+# 1 3 5 7 9 will form the first row
+write(t(x), fil)
+if(interactive()) file.show(fil)
+unlink(fil) # tidy up
+
+# Writing to the "console" 'tab-delimited'
+# two rows, five cols but the first row is 1 2 3 4 5
+write(x, "", sep = "\t")
